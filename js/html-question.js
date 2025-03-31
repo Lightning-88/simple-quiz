8ed519b22684d4f549d3bf9fccd6afd260b31c72
Lightning-88 <erlanggasatria6677@gmail.com>
2025-03-31 22:21:07 +0700

    Add files via upload

---
+++ js/html-question.js
@@ -0,0 +1,91 @@
+const questions = [
+  {
+    question:
+      "Manakah elemen semantic HTML5 yang paling tepat untuk mendefinisikan area navigasi utama pada sebuah halaman web?",
+    options: ["&lt;div&gt;", "&lt;section&gt;", "&lt;nav&gt;", "&lt;aside&gt;"],
+    answer: 2,
+  },
+  {
+    question: "Dalam penggunaan <canvas> di HTML5, apa tujuan utamanya?",
+    options: [
+      "Menyusun layout halaman secara responsif",
+      "Menggambar grafik dinamis menggunakan JavaScript",
+      "Menampilkan konten video secara streaming",
+      "Mengintegrasikan file SVG ke dalam halaman",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Elemen <figure> dan <figcaption> digunakan bersamaan untuk?",
+    options: [
+      "Menyematkan video dan keterangan",
+      "Menampilkan gambar atau ilustrasi beserta keterangan",
+      "Membuat layout grid responsif",
+      "Menyimpan data dinamis",
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "Manakah dari berikut ini yang merupakan elemen baru pada HTML5 untuk mendefinisikan artikel independen?",
+    options: [
+      "&lt;main&gt;",
+      "&lt;section&gt;",
+      "&lt;aside&gt;",
+      "&lt;article&gt;",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Elemen <svg> digunakan untuk?",
+    options: [
+      "Menyisipkan video",
+      "Menggambar grafik vektor langsung dalam dokumen HTML",
+      "Menyimpan data XML",
+      "Menggabungkan file CSS dan JavaScript",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Apa kegunaan atribut placeholder pada elemen <input> di HTML5?",
+    options: [
+      "Menentukan tipe data input",
+      "Menyimpan nilai default permanen",
+      "Menghubungkan input dengan label tertentu",
+      "Menyediakan teks petunjuk sementara di dalam kotak input",
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "Manakah atribut yang wajib ada pada tag <img> agar gambar tetap dapat diakses oleh pengguna yang koneksinya jelek?",
+    options: ["alt", "srcset", "title", "src"],
+    answer: 0,
+  },
+  {
+    question: "Dibawah ini yang termasuk element sematic HTML5 adalah...",
+    options: ["&lt;aside&gt;", "&lt;div&gt;", "&lt;bottom&gt;", "Semua Salah"],
+    answer: 0,
+  },
+  {
+    question:
+      "Di HTML5, elemen mana yang sebaiknya digunakan untuk menandai konten yang berisi navigasi tambahan atau sekunder?",
+    options: [
+      "&lt;nav&gt;",
+      "&lt;aside&gt;",
+      "&lt;footer&gt;",
+      "&lt;section&gt;",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Apa fungsi dari elemen <mark> di HTML5?",
+    options: [
+      "Untuk menyisipkan komentar",
+      "Untuk membuat teks menjadi italic",
+      "Untuk menambahkan hyperlink",
+      "Untuk menandai bagian teks yang penting atau relevan dengan konteks",
+    ],
+    answer: 3,
+  },
+];
